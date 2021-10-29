054d5e8345c9052117fe9d3c9e40d1eef9e4d39a
RUnroe <ryanunroe@gmail.com>
2021-10-28 19:23:51 -0600

    Add project files.

---
+++ RyanUnroe_Factory/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RyanUnroe_Factory
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateTextBlock_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveLastElement_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportXAML_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportHTML_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
